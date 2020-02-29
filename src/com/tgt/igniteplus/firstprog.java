910286c4ecb0600a9874de4552219c5cfde0b561
somya1209 <somya123024@gmail.com>
2020-02-29 17:10:29 +0530

    changed print statement

--- src/com/tgt/igniteplus/firstprog.java
+++ src/com/tgt/igniteplus/firstprog.java
@@ -3,6 +3,6 @@ package com.tgt.igniteplus;
 public class firstprog {
 
     public static void main(String[] args) {
-	System.out.println("hello world");
+	System.out.println("hello ignite plus2020");
     }
 }
